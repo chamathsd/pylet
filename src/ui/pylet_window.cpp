6baa8fcbcbcf796ea151a8b9abdd43b0ea99f614
Jake Schadaway <jschadaway@gmail.com>
2015-09-19 18:03:52 -0400

    Add tentative UI layout

--- src/ui/pylet_window.cpp
+++ src/ui/pylet_window.cpp
@@ -6,8 +6,11 @@
 #include "pylet_window.h"
 #include <qapplication.h>
 #include <qdesktopwidget.h>
+#include <qplaintextedit.h>
 #include <qmenubar.h>
 #include <qlayout.h>
+#include <qlabel.h>
+#include <qsplitter.h>
 
 PyletWindow::PyletWindow(QWidget *parent) :
     QMainWindow(parent)
@@ -17,7 +20,7 @@ PyletWindow::PyletWindow(QWidget *parent) :
 
     initWindow();
     initWidgets();
-    show();
+    showMaximized();
 }
 
 void PyletWindow::initWindow()
@@ -26,7 +29,6 @@ void PyletWindow::initWindow()
     int screenWidth = screenRect.width();
     int screenHeight = screenRect.height();
     resize(screenWidth - 250, screenHeight - 250);
-    showMaximized();
 
     setWindowTitle("Pylet (Editor)"); 
 }
@@ -34,13 +36,51 @@ void PyletWindow::initWindow()
 void PyletWindow::initWidgets()
 {
     populateMenu();
-
-    QVBoxLayout *mainLayout = new QVBoxLayout(this);
-    setLayout(mainLayout);
-
-    mainLayout->addStretch();
-
+    addToolBar("Action Bar");
     statusBar();
+
+    QSplitter* coreWidget = new QSplitter(Qt::Horizontal);
+    setCentralWidget(coreWidget);
+
+    QWidget* navigator = new QWidget(coreWidget);
+    QVBoxLayout* navLayout = new QVBoxLayout(navigator);
+    navLayout->setMargin(0);
+    navLayout->setSpacing(0);
+    navigator->setLayout(navLayout);
+    navigator->setMinimumWidth(280);
+    coreWidget->insertWidget(0, navigator);
+
+    QLabel* crawler = new QLabel(navigator);
+    crawler->setStyleSheet("background-color: red; color: white; font-size: 40px;");
+    crawler->setAlignment(Qt::AlignCenter);
+    crawler->setText("File Crawler");
+    navLayout->addWidget(crawler, 3);
+
+    QLabel* infoBox = new QLabel(navigator);
+    infoBox->setStyleSheet("background-color: green; color: white; font-size: 40px;");
+    infoBox->setAlignment(Qt::AlignCenter);
+    infoBox->setText("Info Box");
+    navLayout->addWidget(infoBox, 1);
+
+    QLabel* codeEditor = new QLabel(coreWidget);
+    codeEditor->setStyleSheet("background-color: blue; color: white; font-size: 40px;");
+    codeEditor->setAlignment(Qt::AlignCenter);
+    codeEditor->setText("Code Editor");
+    codeEditor->setMinimumWidth(280);
+    coreWidget->insertWidget(1, codeEditor);
+
+    QLabel* shell = new QLabel(coreWidget);
+    shell->setStyleSheet("background-color: orange; color: white; font-size: 40px;");
+    shell->setAlignment(Qt::AlignCenter);
+    shell->setText("Shell / Tracer");
+    shell->setMinimumWidth(280);
+    coreWidget->insertWidget(2, shell);
+
+    coreWidget->setStretchFactor(0, 1);
+    coreWidget->setStretchFactor(1, 5);
+    coreWidget->setStretchFactor(2, 3);
+
+    coreWidget->setMidLineWidth(8);
 }
 
 void PyletWindow::populateMenu()
