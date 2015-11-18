7deb06ba7a07a127d950e737676997d21d400c81
Jake Dharmasiri <jschadaway@gmail.com>
2015-11-17 22:49:25 -0500

    Implement introductory logic for embedded Python interpreter

---
+++ src/ui/console.h
@@ -0,0 +1,18 @@
+/*
+* Copyright (c) 2015 Jake Schadaway.
+* Licensed under the MIT License. See LICENSE.md for details.
+*/
+
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+#include <qplaintextedit.h>
+
+class Console : public QPlainTextEdit {
+    Q_OBJECT
+
+public:
+    Console(QWidget *parent = 0);
+};
+
+#endif // CONSOLE_H
